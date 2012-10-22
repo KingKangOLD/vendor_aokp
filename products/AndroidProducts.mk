88376862b241c55afb85761f4f31cfd991f5ae1d
DAGr8 <renocustom@gmail.com>
2012-10-22 13:49:18 -0400

    add overlay and stuff for skyrocket
    Change-Id: Ia3687a119dbd66625f3bcd5f01153f23a932a5de
    
    Conflicts:
    
    	products/AndroidProducts.mk
    	vendorsetup.sh

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -8,5 +8,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/skyrocket.mk \
+<<<<<<< HEAD
+=======
+    $(LOCAL_DIR)/tf101.mk \
+>>>>>>> ce0b79d... add overlay and stuff for skyrocket
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/toroplus.mk
