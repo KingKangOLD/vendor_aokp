ed16344b14103c3dab8bd479537422936a7f4622
Rohan Mathur <rohanmathur34@gmail.com>
2012-11-12 15:54:30 -0500

    products: derp

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,10 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/hercules.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/quincyatt.mk \
-    $(LOCAL_DIR)/skyrocket.mk \
-<<<<<<< HEAD
-=======
+    $(LOCAL_DIR)/skyrocket.mk \=
     $(LOCAL_DIR)/tf101.mk \
->>>>>>> ce0b79d... add overlay and stuff for skyrocket
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/toroplus.mk
