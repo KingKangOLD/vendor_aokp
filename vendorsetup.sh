56cadff51d4779494d17579b2132973e5a984b47
Rohan Mathur <rohanmathur34@gmail.com>
2012-11-12 15:53:48 -0500

    vendorsetup: derp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,9 +6,6 @@ add_lunch_combo aokp_hercules-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_quincyatt-userdebug
 add_lunch_combo aokp_skyrocket-userdebug
-<<<<<<< HEAD
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_toroplus-userdebug
-=======
 add_lunch_combo aokp_vibrantmtd-userdebug
->>>>>>> ce0b79d... add overlay and stuff for skyrocket
